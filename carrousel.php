8d19ac7f8718d6efbd7a82c358b13dff409a36b6
sadek <bilal2dellys@hotmail.com>
2022-07-07 21:07:01 -0400

    nouvelle version du prof pour s'assurer de code

---
+++ carrousel.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: Caroussel
+ * Author: Saddek Touati
+ * Description: Carrousel d'image créé à partir d'une galerie
+ * Plugin URI: https://github.com/sadektouati/31w-extension-tp1
+ */
+
+function carrousel_31w_enqueue()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) ."style.css");
+    $version_js =  filemtime(plugin_dir_path(__FILE__) ."script/carrousel.js");
+    // var_dump(__FILE__); die();
+
+    wp_enqueue_style('carrousel_31w_css',
+                     plugin_dir_url(__FILE__) . "style.css", 
+                     array(), 
+                     $version_css);
+
+    wp_enqueue_script('carrousel_31w_js',
+                    plugin_dir_url(__FILE__) . "script/carrousel.js",
+                    array(), 
+                    $version_js,
+                    true);
+}
+add_action('wp_enqueue_scripts', "carrousel_31w_enqueue");
+
+
+
+function genere_carrousel(){
+    ///////////////////////////////HTML
+    $contenu = '<div class="carrousel">';
+    $contenu .= '<button class="carrousel__fermeture">X</button>';
+    $contenu .= '<figure class="carrousel__figure"></figure>';
+    $contenu .= '<form class="carrousel__radio"></form>';
+    $contenu .= '</div><!-- fin du caroussel -->';
+
+    return $contenu;
+
+}
+
+add_shortcode('carrousel', 'genere_carrousel');
---
+++ script/carrousel.js
@@ -0,0 +1,87 @@
+/**
+ * Fonction IIFE qui permet de contrôler l'affichage du carrousel
+ * Auteur : Eddy Martin
+ * Date de dernière modification : 6 juillet 2022
+ * 
+ */
+
+
+ (function(){
+    console.log('Début du carrousel')
+    let elmGalerie = document.querySelector('.galerie')
+    let elmGalerieImg = elmGalerie.querySelectorAll("figure figure img")
+    let elmCarrousel = document.querySelector('.carrousel')
+    let elmCarrousel__figure = document.querySelector('.carrousel__figure')
+    let elmCarrousel__fermeture = document.querySelector('.carrousel__fermeture')
+    let elmCarrousel__radio = document.querySelector('.carrousel__radio')
+    console.log(elmGalerieImg[0].getAttribute('src'))
+    let index = 0 // L'index qui permettra d'indexer les images du carrousel et les radios bouton
+    let ancien_index = -1
+    let compteur =0
+    for (const img of elmGalerieImg){
+        img.dataset.index = compteur++
+        ajouter_elmImg(img)
+        ajouter_elmRadio()
+        // écouteur sur la galerie pour ouvrir le carrousel
+        img.addEventListener('mousedown', function(){
+            elmCarrousel.classList.add('carrousel__ouvrir') 
+            index = this.dataset.index
+            affiche_carrousel_img()
+    }) 
+    }
+/**
+ * Écouteur pour fermer le carrousel
+ * 
+ */
+    elmCarrousel__fermeture.addEventListener('mousedown', function(){
+        elmCarrousel.classList.remove('carrousel__ouvrir') 
+    })
+
+/**
+ * Ajouter un img créé dynamiquement dans le conteneur elmCarrousel__figure
+ * 
+ * @param {ChildNode} img   Une image de la galerie
+ */
+    function ajouter_elmImg(img){
+        let elmImg = document.createElement('img') // création d'un élément img
+        elmImg.classList.add('carrousel__figure__img') // ajout d'une classe css
+        elmImg.setAttribute('src', img.getAttribute('src')) // on récupère l'adresse URL de l'image de la galerie
+        elmCarrousel__figure.appendChild(elmImg) // ajouter l'élément img dans le carrousel
+        elmImg.dataset.index = index // on index l'élément img pour pouvoir l'associé au bon radio bouton
+    }
+/**
+ * Ajouter un bouton radio créé dynamiquement dans le conteneur elmCarrousel__radio. Ce radio bouton permettra de
+ * naviguer dans le carrousel d'une image à l'autre
+ * 
+ * @param aucun
+ * 
+ * */
+
+    function ajouter_elmRadio(){
+        let elmRadio = document.createElement('input')
+        elmRadio.setAttribute('type','radio')
+        elmRadio.setAttribute('name','radImg')
+        elmRadio.dataset.index = index
+        index = index+1
+        elmCarrousel__radio.appendChild(elmRadio)
+        /* Écouteur pour sélectionner une nouvelle image */ 
+        elmRadio.addEventListener('mousedown', function(){    
+            index = this.dataset.index
+            affiche_carrousel_img()
+        })
+    }
+
+function affiche_carrousel_img()
+{
+    if (ancien_index != -1){
+        elmCarrousel__figure.children[ancien_index].classList.remove('carrousel__figure__img--activer') 
+    }
+    elmCarrousel__figure.children[index].classList.add('carrousel__figure__img--activer')
+    ancien_index = index
+}
+    
+  
+
+
+
+})()
